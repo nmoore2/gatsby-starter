bf59cbd4a646003083ce262b87137164424a121e
Nathan Moore <n@nmoore.net>
2022-09-14 12:55:18 -0600

    more nav, home hero

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -24,13 +24,13 @@ const Nav = () => {
               alt="KeepKey logo"
               src="../images/logos/keepkey_logo.png"
               width={80}
-              className="mr-3"
+              className="mr-5"
               >
       </StaticImage>
-      <a className="nav__link">Community</a>
+      <a href="/" className="nav__link">Community</a>
       <a href="/supported-applications" className="nav__link">Supported Applications</a>
-      <a href="/supported-applications" className="nav__link">Supported Applications</a>
-      <a href="/supported-applications" className="nav__link">Supported Applications</a>
+      <a href="/integration-guide" className="nav__link">Integration Guide</a>
+      <a href="/onboarding" className="nav__link">Onboarding</a>
       </div>
         <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg class="ml-1 w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
         <div id="dropdownNavbar" className="hidden z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,18 +13,31 @@ const HomePage = () => {
   return (
     <Layout>
         <div className="hero hero__home flex items-center" style={{backgroundImage: `url(${background})`}}>
-          <div className="container mx-auto px-4">
-            <h1 className="text-3xl font-bold text-white">The Next Frontier of Crypto Security</h1>
-            <p className="text-white">Protect your cryptocurrencies, store your private keys offline, and safeguard your 
-              assets from hackers. It’s time to achieve financial freedom in the most secure way with KeepKey.</p>
-            <Link to="/" className="btn btn-primary">Buy Now</Link>
+          <div className="container mx-auto px-4 relative grid grid-cols-2 items-center">
+            <div className="hero__text">
+              <h1 className="hero__title font-bold text-white">The Next Frontier of Crypto Security</h1>
+              <p className="hero__description text-white">Protect your cryptocurrencies, store your private keys offline, and safeguard your 
+                assets from hackers. It’s time to achieve financial freedom in the most secure way with KeepKey.</p>
+              {/* <Link to="/" className="btn btn-primary">Buy Now</Link> */}
+              <a href="/" className="btn btn-primary">Buy Now</a>
+            </div>
+            <StaticImage
+              alt="KeepKey Wallet"
+              src="../images/hardware/wallet-hero.png"
+              width={800}
+              height={1200}
+              quality={100}
+              className="hero__image"
+              >
+            </StaticImage>
+
           </div>
         </div>
     </Layout>
   )
 }
 
-export const Head: HeadFC = () => <title>Best Home Page</title>
+// export const Head: HeadFC = () => <title>Home</title>
 
 export default HomePage
 
